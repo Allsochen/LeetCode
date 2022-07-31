a5899b0fb120118157bf9291d7884567196cd83c
allsochen <erxiao.chen.gd@gmail.com>
2022-07-31 16:34:41 +0800

    feat: add topic

---
+++ src/main/java/leetcode/editor/cn/LongestValidParentheses.java
@@ -0,0 +1,89 @@
+//给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。 
+//
+// 
+//
+// 
+// 
+// 示例 1： 
+// 
+// 
+//
+// 
+//输入：s = "(()"
+//输出：2
+//解释：最长有效括号子串是 "()"
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = ")()())"
+//输出：4
+//解释：最长有效括号子串是 "()()"
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = ""
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= s.length <= 3 * 10⁴ 
+// s[i] 为 '(' 或 ')' 
+// 
+//
+// Related Topics 栈 字符串 动态规划 👍 1929 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class LongestValidParentheses {
+
+    public static void main(String[] args) {
+        Solution solution = new LongestValidParentheses().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int longestValidParentheses(String s) {
+            int left = 0, right = 0, maxAns = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    left++;
+                } else {
+                    right++;
+                }
+                if (left == right) {
+                    maxAns = Math.max(maxAns, 2 * right);
+                } else if (right > left) {
+                    left = right = 0;
+                }
+            }
+            left = right = 0;
+            for (int i = s.length() - 1; i >= 0; i--) {
+                if (s.charAt(i) == '(') {
+                    left++;
+                } else {
+                    right++;
+                }
+                if (left == right) {
+                    maxAns = Math.max(maxAns, 2 * left);
+                } else if (left > right) {
+                    left = right = 0;
+                }
+            }
+            return maxAns;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
